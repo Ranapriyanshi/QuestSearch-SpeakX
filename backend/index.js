13a0863e85eafebb18882e50983f29b7d4a56336
Priyanshi Rana <priyanshi.rana@napses.com>
2025-01-27 15:16:54 +0530

    bugFix

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const { startGRPCServer } = require("./grpc/server");
 const { connectToDatabase } = require("./utils/db");
+const app = require("./app");
+
+const PORT = process.env.PROXY_PORT || 5000;
+
+// Start the Express server
 
 // Start both servers
 const startApp = async () => {
@@ -10,7 +15,9 @@ const startApp = async () => {
   startGRPCServer();
 
   // Start proxy server
-  require("./proxy/server");
+  app.listen(PORT, () => {
+    console.log(`Proxy server running at http://localhost:${PORT}`);
+  });
 };
 
 startApp();
