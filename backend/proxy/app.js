5c06936e80bb94ccd207d18f89a51746eeb2eb2d
Priyanshi Rana <priyanshi.rana@napses.com>
2025-01-27 10:44:52 +0530

    cors fix

--- backend/proxy/app.js
+++ backend/proxy/app.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const corsMiddleware = require("./middleware/corsMiddleware");
 const searchRoutes = require("./routes/searchRoute");
+const cors = require("cors");
 
 const app = express();
 
 // Apply middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://quest-search-speak-x.vercel.app",
-    ],
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // If cookies or Authorization headers are used
-  })
-);
+app.use(corsMiddleware);
 app.use(express.json());
 
 // Add routes
