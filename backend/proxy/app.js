b16fe8fbbc787d6d5241ac5c8ae3406a0abb16c8
Priyanshi Rana <priyanshi.rana@napses.com>
2025-01-27 11:00:47 +0530

    minor fix

--- backend/proxy/app.js
+++ backend/proxy/app.js
@@ -11,11 +11,11 @@ app.use(express.json());
 // Add routes
 app.use("/api", searchRoutes);
 
-// app.options("*", (req, res) => {
-//   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.sendStatus(204); // Respond OK without content
-// });
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(204); // Respond OK without content
+});
 
 module.exports = app;
