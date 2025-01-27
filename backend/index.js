2259f41123d08df43b6e1a9783028f73288d6b1a
Priyanshi Rana <priyanshi.rana@napses.com>
2025-01-27 15:27:21 +0530

    vercel.json file added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,6 @@
 require("dotenv").config();
 const { startGRPCServer } = require("./grpc/server");
 const { connectToDatabase } = require("./utils/db");
-const app = require("./proxy/app");
-
-const PORT = process.env.PROXY_PORT || 5000;
-
-// Start the Express server
 
 // Start both servers
 const startApp = async () => {
@@ -15,9 +10,7 @@ const startApp = async () => {
   startGRPCServer();
 
   // Start proxy server
-  app.listen(PORT, () => {
-    console.log(`Proxy server running at http://localhost:${PORT}`);
-  });
+  // require("./proxy/server");
 };
 
 startApp();
